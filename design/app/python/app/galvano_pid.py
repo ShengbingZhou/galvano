5eccefe4ac9a9a47cb0a70ababd6b1bb32ab6248
Shengbing Zhou <shengbingzhou@outlook.com>
2019-05-08 13:24:44 +0800

    adjust some params and draw adc & dac on same plot

--- design/app/python/app/galvano_pid.py
+++ design/app/python/app/galvano_pid.py
@@ -3,6 +3,7 @@
 # @author:  shengbingzhou@outlook.com
 
 import sys
+import time
 import array
 import numpy
 import scipy
@@ -46,13 +47,13 @@ def get_xp_adc():
     data_out = array.array('B', [ 0x01, 0x00, 0x00, 0x00, 0x00 ]) # nop command
     (count, data_in) = aardvark_py.aa_spi_write(pid.aardvark, data_out, data_in)
     return (data_in[1] << 8) + data_in[2]
-    
+
 def set_xi_dac(value):
-    value = (value / 2) & 0xffff
+    value = (value / 2) & 0xffff # cover fpga bug: dac will shift 1 more bit when loading data
     aardvark_py.aa_spi_write(pid.aardvark, array.array('B', [ 0x02, (value >> 8), (value & 0xff) ]), 0) # 300us
 
 class pid_control:
-    def __init__(self, P=200.0, I=15.0, D=200.0, derivator=0, integrator=0, integrator_max=3000, integrator_min=-3000):
+    def __init__(self, P=0.003, I=0.003, D=0.00, derivator=0, integrator=0, integrator_max=3000, integrator_min=-3000):
         self.Kp = P
         self.Ki = I
         self.Kd = D
@@ -86,42 +87,47 @@ class pid_control:
         self.D_value = self.Kd * ( self.error - self.derivator)
         self.derivator = self.error
         self.integrator = self.integrator + self.error
-        self.integrator = self.integrator_max if self.integrator > self.integrator_max else self.integrator
-        self.integrator = self.integrator_min if self.integrator < self.integrator_min else self.integrator
+        #self.integrator = self.integrator_max if self.integrator > self.integrator_max else self.integrator
+        #self.integrator = self.integrator_min if self.integrator < self.integrator_min else self.integrator
         self.I_value = self.integrator * self.Ki
         return self.P_value + self.I_value + self.D_value
 
     def set_point(self, point):
-        self.setpoint  = point
+        self.setpoint   = point
         self.integrator = 0
         self.derivator  = 0
 
 # instantiate pid controller
 pid = pid_control()
 
+# release motor
+set_xi_dac(0x8000)
+time.sleep(1)
+
 # init xp adc
 init_xp_adc()
 
 # xp data and current array
-xp_data = [0] * 2000
-xi_dac  = [0] * 2000
+xp_data = [0] * 1000
+xi_dac  = [0] * 1000
 
 # draw xp adc data
 fig = matplotlib.pyplot.figure(figsize=(7, 7))
-ax_data_bin = fig.add_subplot(2, 1, 1)
+ax_data_bin = fig.add_subplot(1, 1, 1)
 ax_data_bin.grid()
-ax_data_bin.set_ylim(0, 65535)
+ax_data_bin.set_ylim(-5000, 65635 + 5000)
 ax_data_bin.set_ylabel('Data')
-line_data_bin, = ax_data_bin.plot(xp_data, 'g')
+line_data_bin, = ax_data_bin.plot(xp_data, 'g')    # green line
 ax_data_value = ax_data_bin.twinx()                # show another Y-axis (voltage value)
-ax_data_value.set_ylim(-10.24, 10.24)
-ax_data_value.set_ylabel('Value')
-#animation = matplotlib.animation.FuncAnimation(fig, update, interval=10) # show adc data waveform
+ax_data_value.set_ylim(-10.24 - 1.5625, 10.24 + 1.5625)
+ax_data_value.set_ylabel('V (ADC)')
+#animation = matplotlib.animation.FuncAnimation(fig, update, interval=10) # show adc data waveform like oscilloscope
+#matplotlib.pyplot.show()
 
 # get xp adc data (test)
 # for i in range(len(xp_data)): 
-    # xp_data[i] = get_xp_adc() 
-# line.set_ydata(xp_data) 
+    # xp_data[i] = get_xp_adc()
+# line.set_ydata(xp_data)
 
 # draw xp adc data histogram
 #ax_data_hist = fig.add_subplot(2, 1, 2)
@@ -132,25 +138,27 @@ ax_data_value.set_ylabel('Value')
 #for i in range(0xff):
 #    set_xi_dac(i << 8)
 
-# release motor
-set_xi_dac(0x8000)
-
 # pid algorithm
-pid.set_point(10)
+pid.set_point(32700)
 for i in range(len(xp_data)):
     xp_data[i] = get_xp_adc()
     out = pid.update(xp_data[i])
-    xi_dac[i] = int(out)
+    out = int(out)
+    out = out + 0x8000
+    out = 65535 if out > 65535 else out
+    out = 0     if out < 0     else out
+    xi_dac[i] = out
     set_xi_dac(xi_dac[i])
 
 # show x motor position waveform
 line_data_bin.set_ydata(xp_data)
 
 # show dac's output waveform
-ax_dac_out = fig.add_subplot(2, 1, 2)
-ax_dac_out.grid()
-ax_dac_out.set_ylim(0, 65535)
-ax_dac_out.plot(xi_dac)
+ax_data_bin.plot(xi_dac, 'r') # red line
+
+# release motor
+set_xi_dac(0x8000)
+time.sleep(1)
 
 # show xp adc figure
 matplotlib.pyplot.show()
